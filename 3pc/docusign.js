735ce733f44a410acf24dd3e1d0a185a4f7d2586
Renan Araújo <renana.dstest@gmail.com>
2024-03-09 17:18:52 -0300

    3PC initial commit

---
+++ 3pc/docusign.js
@@ -0,0 +1,111 @@
+var docusign = {
+    accessToken: null,
+    accountId: null,
+    envelopeId: null,
+    baseUrl: null,
+    debug: null,
+    auth: function (token) {
+      this.accessToken = token;
+      return this;
+    },
+    config: function ({ accountId, baseUrl, debug }) {
+      this.accountId = accountId ? accountId : this.accountId;
+      this.baseUrl = baseUrl ? baseUrl : this.baseUrl;
+      this.debug = debug ? debug : this.debug;
+      return this;
+    },
+    envelope: function (envelopeId) {
+      this.envelopeId = envelopeId;
+      return this;
+    },
+    debugResult: function (json) {
+      console.log(json);
+      return this;
+    },
+    userinfo: async function () {
+      let response = await fetch(
+        "https://account-d.docusign.com/oauth/userinfo",
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${this.accessToken}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      let json = await response.json();
+
+      if (this.debug) this.debugResult(json);
+
+      return json;
+    },
+    create: async function (path) {
+      let response = await fetch(
+        `https://demo.docusign.net/restapi/v2.1/accounts/${this.accountId}/${path}`,
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            Authorization: `Bearer ${this.accessToken}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            emailSubject: "Third-Party Cookies (test envelope)",
+            status: "created",
+          }),
+        }
+      );
+
+      let json = await response.json();
+
+      if (this.debug) this.debugResult(json);
+
+      return json;
+    },
+    view: async function (path) {
+      let response = await fetch(
+        `https://demo.docusign.net/restapi/v2.1/accounts/${this.accountId}/envelopes/${this.envelopeId}/views/${path}`,
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            Authorization: `Bearer ${this.accessToken}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            returnUrl: "https://httpbin.org/post",
+          }),
+        }
+      );
+
+      let json = await response.json();
+
+      if (this.debug) this.debugResult(json);
+
+      return json;
+    },
+    void: async function () {
+      let response = await fetch(
+        `https://demo.docusign.net/restapi/v2.1/accounts/${this.accountId}/envelopes/${this.envelopeId}`,
+        {
+          method: "PUT",
+          mode: "cors",
+          headers: {
+            Authorization: `Bearer ${this.accessToken}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            status: "voided",
+            voidedReason: "Deleting the test envelope after loading it.",
+          }),
+        }
+      );
+
+      let json = await response.json();
+
+      if (this.debug) this.debugResult(json);
+
+      return json;
+    },
+  };
---
+++ 3pc/init.js
@@ -0,0 +1,69 @@
+var refreshBtn = document.getElementById("refreshButton");
+var voidBtn = document.getElementById("voidButton");
+
+window.onload = async function () {
+  var token = "";
+
+  if (!!window.location.hash) {
+    token = window.location.hash.slice(1).split("&")[0].split("=")[1];
+    window.sessionStorage.setItem("app-dsat", token);
+    window.location.hash = "";
+  }
+
+  token = window.sessionStorage.getItem("app-dsat");
+
+  var greeting = "";
+  var message = "";
+  var envelopeVoided = false;
+
+  var dsStatus = document.getElementById("docusign-status");
+  var dsFrame = document.getElementById("docusign-iframe");
+
+  message =
+    "Connect your demo account <a href='https://account-d.docusign.com/oauth/auth?response_type=token&scope=signature cors&client_id=ac4f43c8-49ef-4665-ae45-978983eea3b7&redirect_uri=http://localhost:5500/third-party.html'>here</a>";
+
+  if (!!token) {
+    await docusign.auth(token).config({ debug: true });
+
+    userData = await docusign.userinfo();
+
+    let { given_name } = userData;
+
+    message = `Thank you, ${given_name}. You're good to go!`;
+
+    let { account_id, base_uri } = userData.accounts.find(
+      (account) => (account.is_default = true)
+    );
+
+    docusign.config({
+      accountId: account_id,
+      baseUrl: base_uri,
+    });
+
+    let envelope = await docusign.create("envelopes");
+
+    let { envelopeId } = envelope;
+
+    let { url } = await docusign.envelope(envelopeId).view("sender");
+
+    dsFrame.src = url;
+
+    refreshBtn.addEventListener("click", async () => {
+      if (!envelopeVoided) {
+        console.log("Voiding envelope...");
+        await docusign.envelope(envelopeId).void();
+      }
+
+      console.log("Refreshing page...");
+      location.reload();
+    });
+
+    voidBtn.addEventListener("click", async () => {
+      console.log("Voiding envelope...");
+      await docusign.envelope(envelopeId).void();
+      envelopeVoided = true;
+    });
+  }
+
+  dsStatus.innerHTML = message;
+};
